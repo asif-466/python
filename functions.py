7c6d7d73e89cd8c2afeb1b3a1fa07a6c6548d0a3
asif <uasif466@gmail.com>
2024-08-15 17:49:30 +0530

    change in repo

--- functions.py
+++ functions.py
@@ -4,9 +4,41 @@
 #     return sum
 # cal(5,6,4)
 
-l=["delhi","gurgaon","kolkata","goa"]
-l2=["1","2","3","4","5"]
-def print_len(list):
-    print(len(list))
-print_len(l)
-print_len(l2)
+# l=["delhi","gurgaon","kolkata","goa"]
+# l2=["1","2","3","4","5"]
+# print(l2[2])
+# def print_len(list):
+#     print(len(list))
+# print_len(l)
+# print_len(l2)
+# l=["delhi","gurgaon","kolkata","goa"]
+# def list(list):
+#     for i in list:
+#         print(i,end=" ")
+    
+# list(l)
+# print()
+# def fac(num):
+#     # num=int(input("enter any number"))
+#     fac=1
+#     for i in range(1,num +1):
+#         fac*=i
+#     print(fac)
+# fac(6)
+
+# def con(usd):
+#     # usd=int(input("enter usd :"))
+#     c=usd*83
+#     print(c,"this is inr:")
+
+
+# con(5)
+
+# def check():
+#     num=int(input("enter any number:"))
+#     if num%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+
+# check()
