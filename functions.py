c94734dd5a5c57414dd7ce4a046bd442d52f607d
asif <uasif466@gmail.com>
2024-08-15 16:22:33 +0530

    change in repo

---
+++ functions.py
@@ -0,0 +1,12 @@
+# def cal(a,b,c):
+#     sum=(a + b + c)/3
+#     print(sum)
+#     return sum
+# cal(5,6,4)
+
+l=["delhi","gurgaon","kolkata","goa"]
+l2=["1","2","3","4","5"]
+def print_len(list):
+    print(len(list))
+print_len(l)
+print_len(l2)
