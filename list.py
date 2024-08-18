43cc897418fff759127adfa3ca35c34aba348625
asif <uasif466@gmail.com>
2024-08-18 18:42:26 +0530

    change in repo

--- list.py
+++ list.py
@@ -1,28 +1,33 @@
-# #indexing and slicing of list
+##indexing and slicing of list
 # list1=["hello","my","lovely","baby"]
 # index=list[0]
-# list2=["no","bro"]
-# list1[0:2]=list2
-# print(list2)
-# print(list1)
+# print(index)
 # l1=["monu","a","a","m","jonu"]
 # a=list1[0] +" "+ list1[4]
 # b=list1[0] [2]
 # print(a)
 # print(b)
-#inserting of list
+##changing list item
+# l1=["hello","my","lovely","baby"]
+# l1[0]="world"
+# print(l1)
+# list1=["hello","my","lovely","baby"]
+# list2=["no" ,"bro"]
+# list1[0:2]=list2
+# print(list1)
+## inserting of list
 # insertlist=["hello","baby","what","are","you","doing"]
 # insertlist.insert(1,"world")
 # print(insertlist)
-#apending of list
+# # apending of list
 # appendlist=["hello","baby","what","are","you","doing"]
 # appendlist.append("e")
 # print(appendlist)
-#removeing of list from element name
+# # removeing of list from element name
 # removelist=["hello","baby","what","are","you","doing"]
 # removelist.remove("baby")
 # print(removelist)
-#removing from index no
+# # removing from index no
 # poplist=["hello","baby","what","are","you","doing"]
 # poplist.pop(1)
 # print(poplist)
@@ -34,22 +39,22 @@
 # l=[88,33,44,55,66]
 # s=sorted(l, reverse=True)
 # print(s)
-# #for loop for list
-l=[88,33,44,55,66]
-for i in l:
-     print(i,end=" \n")#print same line with end
-#while loop for list
-w=[1,2,3,4,5]
-ind=0
-while ind <len(w):
-     print(w[ind])
-     ind=ind +1
-# list for square loop
-a=[1,2,3,4,5]
-suare=[num**2 for num in a]
-print("print after suare",suare)
-# list for loop with index no
+# # #for loop for list
+# l=[88,33,44,55,66]
+# for i in l:
+#      print(i,end=" \n")#print same line with end
+# #while loop for list
+# w=[1,2,3,4,5]
+# ind=0
+# while ind <len(w):
+#      print(w[ind])
+#      ind=ind +1
+# # list for square loop
+# a=[1,2,3,4,5]
+# suare=[num**2 for num in a]
+# print("print after suare",suare)
+# # list for loop with index no
 
-fruite=["apple","banana","mango"]
-for index, i in enumerate(fruite):
-     print("print with index no",index,i)
+# fruite=["apple","banana","mango"]
+# for index, i in enumerate(fruite):
+#      print("print with index no",index,i)
--- list_exercise.py
+++ list_exercise.py
@@ -1,15 +1,7 @@
-
-# str="hello this a loop"
-# a=str[0:5] + str[13:18]
-# list1.remove("a")
-# list1.insert(1,1)
-# print(list1)
-# print(len(list))
-# list[0]=10
-# print(list)
-# print(list[-3:])
-# list.append(6)
-# list.sort(reverse=True)
-# re=list1[::-1]
+list1=["hello"]
+re=list1.copy()
+re=list1[::-1]
+print(list1)
+print(re)
 # re=list1.copy()
 # print(a)
