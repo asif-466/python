4772a4151f96742886888553f565814be431084d
asif <uasif466@gmail.com>
2024-08-21 20:14:59 +0530

    change in repo

--- work.py
+++ work.py
@@ -3,14 +3,16 @@ a = a.split(" ")
 firstlen = len(a[0])
 count=0
 s=0
+bb = ""
 for i in a:
     length = len(i)
     if length > firstlen:
-        print("phle str se bade wale str :",i)
+        bb= bb +" "+ i
     elif length < firstlen:
         count =count + 1
         
     elif length==firstlen:
         s=s + 1
+print("phle wale str se bade wale str:",bb)        
 print("phle str se chote wale str ka count:",count)
 print("phle wale str ke equal len ka count:",s)
