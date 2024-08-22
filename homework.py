6b80eb0b00ff2f855e881cdc8bd663b9147164a1
asif <uasif466@gmail.com>
2024-08-22 19:03:33 +0530

    change in repo

--- homework.py
+++ homework.py
@@ -1,17 +1,10 @@
-a="hello,baby,hhhhhh,rr,e,hello"
-a=a.split(",")
-firstlen=len(a[0])
-count=0
-c=0
-for  i in a:
-        lenght=len(i)
-        if lenght>firstlen:
-                print("pahla str se bada wala str:",i)
-        elif lenght<firstlen:
-                count=count + 1
-        elif lenght==firstlen:
-                print("phla str ke chote wale str ka count:",count)
-                print("phle wale str ke equal wale str ka count",c)
+
+
+for i in range(1,10):
+    if i%2==0:
+        print("even",i)
+    if i%2!=0:
+        print("odd",i)
          
          
         
