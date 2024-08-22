331a095e3007660a8e5dc6b9f24225c3b23bee7c
asif <uasif466@gmail.com>
2024-08-22 19:16:13 +0530

    change in repo

--- homework.py
+++ homework.py
@@ -1,11 +1,13 @@
 
+a=int(input("enter starting no: "))
+b=int(input("enter range lenght: "))
 
-for i in range(1,10):
+for i in range(a,b+1):
     if (i%2==0):
         print("even",i)
+    
+for i in range(a,b+1):
     if (i%2!=0):
         print("odd",i)
-         
-         
         
-        
+
