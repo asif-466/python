b408e02dcb840494c7576330f17ba3fa57c667d4
asif <uasif466@gmail.com>
2024-08-23 17:47:24 +0530

    change in repo

--- list.py
+++ list.py
@@ -2,59 +2,68 @@
 # list1=["hello","my","lovely","baby"]
 # index=list[0]
 # print(index)
-# l1=["monu","a","a","m","jonu"]
 # a=list1[0] +" "+ list1[4]
 # b=list1[0] [2]
 # print(a)
 # print(b)
+
 ##changing list item
 # l1=["hello","my","lovely","baby"]
 # l1[0]="world"
 # print(l1)
 # list1=["hello","my","lovely","baby"]
 # list2=["no" ,"bro"]
-# list1[0:2]=list2
+# list1[0:3]=list2
 # print(list1)
+
 ## inserting of list
 # insertlist=["hello","baby","what","are","you","doing"]
 # insertlist.insert(1,"world")
 # print(insertlist)
-# # apending of list
+
+## apending of list
 # appendlist=["hello","baby","what","are","you","doing"]
 # appendlist.append("e")
 # print(appendlist)
-# # removeing of list from element name
+
+## removeing of list from element name
 # removelist=["hello","baby","what","are","you","doing"]
 # removelist.remove("baby")
 # print(removelist)
-# # removing from index no
+
+## removing from index no
 # poplist=["hello","baby","what","are","you","doing"]
 # poplist.pop(1)
 # print(poplist)
-# #sorting of list
+
+##sorting of list
 # l=[88,33,44,55,66]
 # l.sort()
 # print(l)
-# #sorting from reverse
+
+##sorting from reverse
 # l=[88,33,44,55,66]
-# s=sorted(l, reverse=True)
-# print(s)
-# # #for loop for list
+# l.reverse()
+# print(l)
+
+##for loop for list
 # l=[88,33,44,55,66]
 # for i in l:
-#      print(i,end=" \n")#print same line with end
-# #while loop for list
+#      print(i,end=" ")#print same line with end
+
+##while loop for list
 # w=[1,2,3,4,5]
 # ind=0
 # while ind <len(w):
 #      print(w[ind])
 #      ind=ind +1
-# # list for square loop
+
+## list for square loop
 # a=[1,2,3,4,5]
 # suare=[num**2 for num in a]
 # print("print after suare",suare)
-# # list for loop with index no
 
+## list for loop with index no
 # fruite=["apple","banana","mango"]
 # for index, i in enumerate(fruite):
-#      print("print with index no",index,i)
+# print("print with index no",index,i)
--- new.py
+++
@@ -1,9 +0,0 @@
-a=input("ENTER ANY STR:")
-a=a.split(" ")
-count=0
-for i in a:
-    b=i[0]
-    c=i[-1]
-    if b==c:
-        count=count + 1
-print(count)
